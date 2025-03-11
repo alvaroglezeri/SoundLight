e618c52982e4c973376d06bcd91e4662e19ebbb8
alvaroglezeri <alvaroglezeri@protonmail.com>
2025-03-11 23:24:58 +0100

    Improved DCL and NothingSelectedEx

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,16 +1,35 @@
 class DuplicateElementException(Exception):
+    """
+    WRITE
+    """
     def __init__(self, msg=f'This element is already registered.'):    
         super().__init__(msg)
 
 class NotFoundException(Exception):
+    """
+    WRITE
+    hola tomás
+    """
     def __init__(self, msg=f'This element cannot be found.'):            
         super().__init__(msg)
 
 class InvalidFileException(Exception):
+    """
+    WRITE
+    """
     def __init__(self, msg=f'This file is invalid.'):            
         super().__init__(msg)
 
 class CodecConversionExeption(Exception):
+    """
+    WRITE
+    """
     def __init__(self, msg=f'A codec conversion must be performed.'):
         super().__init__(msg)
-    
+
+class NothingSelectedException(Exception):
+    """
+    WRITE
+    """
+    def __init__(self, msg=f'Nothing is selected.'):
+        super().__init__(msg)
--- src/core/logger.py
+++ src/core/logger.py
@@ -1,22 +1,36 @@
 from abc import ABC, abstractmethod
 from enum import Enum
+import inspect
 
 enable: bool = True
-class CATEGORY(Enum):
+
+class COLORS:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class LOG_CAT(Enum):
     ERROR = 'FAIL'
     INFO = 'INFO'
     WARN = 'WARN'
     SUCCESS = 'SUCC'
 #CATEGORY = Enum('Category', ['ERROR', 'INFO', 'WARN', 'SUCCESS', 'DEBUG'])
 
-#DOCUMENT
-class Logger(ABC):
+class ILogger(ABC):
     def __new__(cls):
         """
         Singleton implementation
+        #WRITE
         """
         if not hasattr(cls, 'instance'):
-            cls.instance = super(Logger, cls).__new__(cls)
+            cls.instance = super(ILogger, cls).__new__(cls)
         return cls.instance
     
     @abstractmethod
@@ -28,18 +42,19 @@ class Logger(ABC):
         pass
     
     @abstractmethod
-    def log(category: CATEGORY, location: str, msg: any) -> None:
+    def log(category: LOG_CAT, location: str, msg: any) -> None:
         pass
 
     @staticmethod
     def isEnabled() -> bool:
         pass
 
-class DCL(Logger):
+class DCL(ILogger):
     """
     Debug Console Logger
     
-    DOCUMENT
+    WRITE
+    """
     """
     @staticmethod
     def log(msg: any) -> None:
@@ -55,6 +70,40 @@ class DCL(Logger):
     def log(category: CATEGORY, location: str, msg: any) -> None:
         if enable:
             print(f'[DEBUG:{category.value}] at {location}: {msg}')
+    """
+
+    @staticmethod
+    def log(category: LOG_CAT, msg: any) -> None:
+        if enable:
+            frame = inspect.currentframe().f_back  # Marco de la función que llamó a log()
+            class_name:str = None
+
+            if "self" in frame.f_locals:  # Si es un método de instancia
+                class_name:str = frame.f_locals["self"].__class__.__name__
+            elif "cls" in frame.f_locals:  # Si es un método de clase
+                class_name:str = frame.f_locals["cls"].__name__
+
+            caller_name: str = frame.f_code.co_name
+
+            # Select terminal color
+            match category:
+                case LOG_CAT.ERROR as cat:
+                    label: str = f'{COLORS.BOLD}{COLORS.FAIL}[DEBUG:{cat.value}]{COLORS.ENDC}'
+                case LOG_CAT.INFO as cat:
+                    label: str = f'{COLORS.BOLD}[DEBUG:{cat.value}]{COLORS.ENDC}'
+                case LOG_CAT.WARN as cat:
+                    label: str = f'{COLORS.BOLD}{COLORS.WARNING}[DEBUG:{cat.value}]{COLORS.ENDC}'
+                case LOG_CAT.SUCCESS as cat:
+                    label: str = f'{COLORS.BOLD}{COLORS.OKGREEN}[DEBUG:{cat.value}]{COLORS.ENDC}'
+                
+                case _:
+                    label: str = f'[DEBUG]'
+                    
+
+            if class_name:
+                print(f'{label} at {class_name}.{caller_name}: {msg}')
+            else:
+                print(f'{label} at {caller_name}: {msg}')
 
     @staticmethod
     def isEnabled() -> bool:
