ce20fe4a7541e02bbbc75d1b5a8de8411fa78ed2
alvaroglezeri <alvaroglezeri@protonmail.com>
2025-03-26 16:37:20 +0100

    Updated .gitignore and house-keeping

--- src/run.py
+++ src/run.py
@@ -2,24 +2,30 @@ import dependencies
 import os
 
 from core.soundlight import SoundLight
+from core.logger import Logger
+
+# TODO: Update function, class and variable names to follow Python code conventions
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
 
+    # We configure the logging to be done in the built-in console
+    Logger.setOutputFunction(print)
+
     try:
         sl = SoundLight()
 
-        #sl.addFileFromPath(r"resources/Oden & Fatzo X Camden Cox - Lady Love.mp3") 
-        #sl.selectFile(0)
-        #sl.analyze()
-        
-        sl.addFileFromPath(r"resources/Oden & Fatzo X Camden Cox - Lady Love.wav") 
-        sl.selectFile(0)
+        sl.addSongFromPath(
+            r"resources/CamelPhat, Yannis, Foals - Hypercolour.mp3")
+        sl.selectSong(0)
+
+        # sl._printSongMetadata()
+
         sl.analyze()
         sl.generate()
         sl.export(r"output/")
-        
-        #for key, value in sl._fm.getSelectedFile().getMetadata().items():
+
+        # for key, value in sl._fm.getSelectedSong().getMetadata().items():
         #    print(f'{key}: {value}')
 
     except Exception as e:
