7876b1f7928d32bed8c69561303c7418c58e38b0
alvaroglezeri <alvaroglezeri@protonmail.com>
2025-06-08 22:58:24 +0200

    Removed UI classes and unnecessary export structures

--- src/ui/gui/__init__.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(f'Loading {__name__}...')
-
-print(f' 𝈩 Base path: {BASE_PATH}')
-print(f' 𝈩 Package name: {__package__}')
-#sys.path.append(BASE_PATH)
