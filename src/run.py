32c078448eea2c5165d9e7315b836004cef3b238
alvaroglezeri <alvaroglezeri@protonmail.com>
2025-06-08 23:13:02 +0200

    Updated requirements to include TOML for the configuration file

--- src/run.py
+++ src/run.py
@@ -2,31 +2,41 @@ import dependencies
 import os
 
 from core.soundlight import SoundLight
+
+# TODO: Refactor code to use the built-in logger
 from core.logger import Logger
 
-# TODO: Update function, class and variable names to follow Python code conventions
+from core.analysis.simpleAlgorithm.simpleAlgorithm import SimpleAlgorithm as SimpleAnalysisAlgorithm
+from core.generation.simpleGenerator.simpleGenerator import SimpleGenerator as SimpleGenerationAlgorithm
+from core.export.daslight5.Daslight5Exporter import Daslight5Exporter
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
 
     # We configure the logging to be done in the built-in console
-    Logger.setOutputFunction(print)
+    Logger.set_output_function(print)
 
     try:
-        sl = SoundLight()
+        sl = SoundLight('soundlight.toml')
 
-        sl.addSongFromPath(
-            r"resources/CamelPhat, Yannis, Foals - Hypercolour.mp3")
-        sl.selectSong(0)
+        sl.add_song_from_path(
+            r"resources/CamelPhat, Yannis, Foals - Hypercolour.wav")
+        sl.select_song(0)
 
-        # sl._printSongMetadata()
+        sl.set_patch(r"resources/patch.json")
 
-        sl.analyze()
-        sl.generate()
-        sl.export(r"output/")
+        sl.set_analysis_algorithm(SimpleAnalysisAlgorithm())
+        sl.set_generation_algorithm(SimpleGenerationAlgorithm())
+        sl.set_export_algorithm(Daslight5Exporter())
+        sl.set_export_path(r"output/")
 
-        # for key, value in sl._fm.getSelectedSong().getMetadata().items():
-        #    print(f'{key}: {value}')
+        sl.run()
+        sl._save_song_data()
 
     except Exception as e:
+        print()
+        print('--------------- STATE ---------------')
+        print()
+        sl._print_song_data()  # type: ignore
+        print()
         raise e
