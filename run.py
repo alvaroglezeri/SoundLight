73cb66eb88510fba1eb3b9c0d75b7bb8925d070e
alvaroglezeri <alvaroglezeri@protonmail.com>
2025-07-07 13:30:17 +0200

    Simplified the launcher script

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ from src.core.soundlight import SoundLight
 # TODO: Refactor code to use the built-in logger
 from src.core.logger import Logger
 
-from src.core.analysis.simpleAlgorithm.simpleAlgorithm import SimpleAlgorithm as SimpleAnalysisAlgorithm
-from src.core.generation.simpleGenerator.simpleGenerator import SimpleGenerator as SimpleGenerationAlgorithm
+from src.core.analysis.simpleAlgorithm.simpleAlgorithm import SimpleAlgorithm
+from src.core.generation.simpleGenerator.simpleGenerator import SimpleGenerator 
 from src.core.export.daslight5.Daslight5Exporter import Daslight5Exporter
 
 if __name__ == "__main__":
@@ -15,28 +15,20 @@ if __name__ == "__main__":
 
     # We configure the logging to be done in the built-in console
     Logger.set_output_function(print)
+    
+    sl = SoundLight('soundlight.toml')
 
-    try:
-        sl = SoundLight('soundlight.toml')
+    sl.add_song_from_path(
+        r"resources/CamelPhat, Yannis, Foals - Hypercolour.wav")
+    sl.select_song(0)
 
-        sl.add_song_from_path(
-            r"resources/CamelPhat, Yannis, Foals - Hypercolour.wav")
-        sl.select_song(0)
+    sl.set_patch_from_path(r"resources/patch.json")
 
-        sl.set_patch_from_path(r"resources/patch.json")
+    sl.set_analysis_algorithm(SimpleAlgorithm())
+    sl.set_generation_algorithm(SimpleGenerator())
+    sl.set_export_algorithm(Daslight5Exporter())
+    sl.set_export_path(r"output/")
 
-        sl.set_analysis_algorithm(SimpleAnalysisAlgorithm())
-        sl.set_generation_algorithm(SimpleGenerationAlgorithm())
-        sl.set_export_algorithm(Daslight5Exporter())
-        sl.set_export_path(r"output/")
-
-        sl.run()
-        sl._save_song_data()
-
-    except Exception as e:
-        print()
-        print('--------------- STATE ---------------')
-        print()
-        sl._print_song_data()  # type: ignore
-        print()
-        raise e
+    sl.run()
+    sl._save_song_data()
+   
